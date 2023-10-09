e7a7b49b7dcc7972e792c788629559a4a2167335
Althafkv <althafkvworks@gmail.com>
2023-10-09 14:48:44 +0530

    RealSky-Server

---
+++ controllers/resdCntrl.js
@@ -0,0 +1,69 @@
+import asyncHandler from "express-async-handler";
+
+import { prisma } from "../config/prismaConfig.js";
+
+export const createResidency = asyncHandler(async (req, res) => {
+  const {
+    title,
+    description,
+    price,
+    address,
+    country,
+    city,
+    facilities,
+    image,
+    userEmail,
+  } = req.body.data;
+
+  console.log(req.body.data);
+
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        address,
+        country,
+        city,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
+
+    
+    res.send({ message: "Residency Created Successfully", residency });
+  } catch (err) {
+    if (err.code === "P2002") {
+      throw new Error("A Residency with address already exist");
+    }
+    throw new Error(err.message);
+  }
+});
+
+export const getAllResidency = asyncHandler(async (req, res) => {
+  try {
+    const residencies = await prisma.residency.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.send(residencies);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+export const getResidency = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const residency = await prisma.residency.findUnique({
+      where: { id },
+    });
+    res.send(residency);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
